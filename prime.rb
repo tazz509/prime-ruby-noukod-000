ceb837bf2facad17c76d77f3328a2dd8fd1bb771
carly <carly@noukod.com>
2018-09-24 15:17:05 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,9 @@
 # Add  code here!
-def prime number
+def prime number = [1,2,3,4,5,6,7,8,9]
   if number % i == 0
     number != prime
   else
     if number % i == 1
       number = prime
+end
 end
